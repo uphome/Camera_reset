73f3b169a31fd65b5c23c92d62e4c969f2f28811
uphome <1822358119@qq.com>
2023-03-28 20:07:58 +0800

    计算欧拉角，并添加TODO

--- camera/main.cpp
+++ camera/main.cpp
@@ -9,12 +9,14 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d.hpp>
 #include <vector>
+#include <cassert>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
 using namespace ::std;
 using namespace ::cv;
 using namespace ::Eigen;
-
+//TODO：完成旋转角度的可视化。  采用欧拉角
+//TODO: 完善计算R，T的方法。
 struct key_img
 {
   Mat image; // 原图
@@ -23,9 +25,23 @@ struct key_img
   Mat dest; // 描述点
 
 };
+bool epipolarConstraintCheck(Mat CameraK, vector<Point2f>& p1s, vector<Point2f>& p2s, Mat R, Mat t)
+ {
+     for(int i = 0; i < p1s.size(); i++)
+     {
+         Mat y1 = (Mat_<double>(3,1)<<p1s[i].x, p1s[i].y, 1);
+         Mat y2 = (Mat_<double>(3,1)<<p2s[i].x, p2s[i].y, 1);
+         //T 的反对称矩阵
+         Mat t_x = (Mat_<double>(3,3)<< 0, -t.at<double>(2,0), t.at<double>(1,0),
+                     t.at<double>(2,0), 0, -t.at<double>(0,0),
+                   -t.at<double>(1,0),t.at<double>(0,0),0);
+         Mat d = y2.t() * CameraK.inv().t() * t_x * R * CameraK.inv()* y1;
+         cout<<"epipolar constraint = "<<d<<endl;
+     }
+ }
 
 void feature_points(Mat met,struct key_img &fe_point)
-{ met.resize(480,640);
+{ //met.resize(480,640);
   fe_point.image= met;
   Mat image_key;
   Mat dest;
@@ -42,10 +58,10 @@ void feature_points(Mat met,struct key_img &fe_point)
     cout<<"--"<<endl;
 
   }*/
-  drawKeypoints(fe_point.image, fe_point.key_point, fe_point.image_key, Scalar(0, 255, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-/*  cv::namedWindow("camera", CV_WINDOW_NORMAL);
-  imshow("camera",fe_point.image_key);
-  waitKey();*/
+  drawKeypoints(fe_point.image, fe_point.key_point, fe_point.image_key, Scalar(0, 255, 0), DrawMatchesFlags::DEFAULT);
+  cv::namedWindow("图上的特征点", CV_WINDOW_NORMAL);
+  imshow("图上的特征点",fe_point.image_key);
+  waitKey();
 
 }
 
@@ -54,46 +70,52 @@ vector<vector<cv::Point2f>> matching(struct key_img new_image, struct key_img ol
   vector<vector<Point2f>> maPoint;
   vector<Point2f>maPoint_old,maPoint_new;
   BFMatcher matcher;
-  Ptr<DescriptorMatcher> matches = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
-  //vector<DMatch>matches;
+  //Ptr<DescriptorMatcher> matches = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
+  vector<DMatch>matches;
   vector<Mat>trian(1,old_image.dest);
   matcher.add(trian);
   matcher.train();
 
+  const float minRatio = 0.7;
+
+
   vector<vector<DMatch>>matchpoints;
   matcher.knnMatch(new_image.dest,matchpoints,2);
   vector<DMatch>goodfeatur;
+
   for (int i =0;i<matchpoints.size();i++)
   {
-    if ((matchpoints[i][0].distance<0.1*matchpoints[i][1].distance)&&(matchpoints[i][1].distance<=350))
+    if (matchpoints[i][0].distance/matchpoints[i][1].distance<=minRatio)
     {
       goodfeatur.push_back(matchpoints[i][0]);
-
     }
+
+
   }
+  cout<<"匹配特征点的筛选比值为"<<minRatio<<endl;
+  cout<<"筛选好之后的匹配特征点数目为"<<goodfeatur.size()<<endl;
+  assert(("筛选好之后的匹配特征点数目小于8对",goodfeatur.size()>=8));
 
   for(int i =0;i<goodfeatur.size();i++)
   {
     maPoint_new.push_back(new_image.key_point[goodfeatur[i].queryIdx].pt);
     maPoint_old.push_back(old_image.key_point[goodfeatur[i].trainIdx].pt);
   }
+
   maPoint.push_back(maPoint_new);
   maPoint.push_back(maPoint_old);
-  cout<<goodfeatur.size()<<endl;
   Mat result_img;
   drawMatches(new_image.image,new_image.key_point,old_image.image,old_image.key_point, goodfeatur,result_img);
   drawKeypoints(old_image.image,old_image.key_point,old_image.image);
   drawKeypoints(new_image.image,new_image.key_point,new_image.image);
 
 
-/*
   namedWindow("匹配结果",WINDOW_NORMAL);
   resizeWindow("匹配结果",500,500);
   imshow("匹配结果",result_img);
 
   waitKey(0);
-  system("pause");
-*/
+ // system("pause");
 
   return maPoint;
 }
@@ -134,13 +156,23 @@ resizeWindow("最终结果",500,500);
   line(img.image,Point(result(0,3)*100+img.image.cols/2,result(1,3)*100+img.image.rows/2),Point(result(0,0)*100+img.image.cols/2,result(1,0)*100+img.image.rows/2),Scalar(0, 255, 255),10);
   //rectangle(img.image, Point(img.image.cols/2+100,img.image.rows/2+100), Point(img.image.cols/2,img.image.rows/2), Scalar(0, 0, 255),5);
   rectangle(img.image, Point(img.image.cols/2,img.image.rows/2), Point(img.image.cols/2+100,img.image.rows/2+100), Scalar(0, 0, 255),5);
- imshow("最终结果",img.image);
+  imshow("最终结果",img.image);
 
 
 waitKey(0);
 system("pause");
 
 }
+
+
+void drawing_rot(Matrix3d R)
+{
+  Vector3d euler;
+  euler=R.eulerAngles(2,1,0);  // ZYX顺序，即先绕x轴roll,再绕y轴pitch,最后绕z轴yaw,0表示X轴,1表示Y轴,2表示Z轴
+  cout<<euler<<endl;
+
+}
+
 
 int main() {
   MatrixXf transfrom(4, 4);
@@ -156,10 +188,10 @@ int main() {
   VideoCapture capture;
   //capture.open("http://admin:123456@192.168.1.105:8081");
 
-
+    MatrixXd ei_R(3,3);
     Mat K = (Mat_<double>(3, 3) << 477.7987, 0, 323.1992, 0, 477.4408, 240.1797, 0, 0, 1);
-    Mat image_address_new = imread( "/home/hu/桌面/UPAN/CLionProjects/untitled/cmake-build-debug/F__downloads_003.png");
-    Mat image_address_old = imread( "/home/hu/桌面/UPAN/CLionProjects/untitled/cmake-build-debug/F__downloads_003.png");
+    Mat image_address_new = imread( "/home/hu/下载/IMG20230328182935.jpg");
+    Mat image_address_old = imread( "/home/hu/下载/IMG20230328182918.jpg");
   // /home/hu/桌面/UPAN/CLionProjects/untitled/cmake-build-debug/F__downloads_001.png
     feature_points(image_address_new, nimage);
     feature_points(image_address_old, oimage);
@@ -167,14 +199,19 @@ int main() {
     Pose = calmatrix(mPoint);
     cout << "R 为" << endl;
     cout << Pose[0] << endl;
-    cout << test << endl;
-    transfrom.block<3, 3>(0, 0) << Pose[0].at<double>(0, 0), Pose[0].at<double>(0, 1), Pose[0].at<double>(0, 2),
+    cout <<"T 为" << Pose[1]<<endl;
+    ei_R<< Pose[0].at<double>(0, 0), Pose[0].at<double>(0, 1), Pose[0].at<double>(0, 2),
         Pose[0].at<double>(1, 0), Pose[0].at<double>(1, 1), Pose[0].at<double>(1, 2),
         Pose[0].at<double>(2, 0), Pose[0].at<double>(2, 1), Pose[0].at<double>(2, 2);
-    transfrom.col(3) << Pose[1].at<double>(0, 0), Pose[1].at<double>(1, 0), Pose[1].at<double>(2, 0), 1;
+  transfrom.block<3, 3>(0, 0)<< Pose[0].at<double>(0, 0), Pose[0].at<double>(0, 1), Pose[0].at<double>(0, 2),
+      Pose[0].at<double>(1, 0), Pose[0].at<double>(1, 1), Pose[0].at<double>(1, 2),
+      Pose[0].at<double>(2, 0), Pose[0].at<double>(2, 1), Pose[0].at<double>(2, 2);
+  transfrom.col(3) << Pose[1].at<double>(0, 0), Pose[1].at<double>(1, 0), Pose[1].at<double>(2, 0), 1;
     cout << test * transfrom << endl;
     result = transfrom * test;
     drawing(result, nimage);
     cout << result << endl;
+    //epipolarConstraintCheck(K,mPoint[0],mPoint[1],Pose[0],Pose[1]);  // 验证对极约束
+  drawing_rot(ei_R);
     waitKey(1);
   }
